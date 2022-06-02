3fc8cb1b190174f5a7af655111b4062ed658ec13
Matthew Spach <af@Matthews-MacBook-Pro-2.local>
2022-06-02 13:43:32 -0700

    git init

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.listen(3000, function() {
-    console.log('listening on Port 3000');
-    console.log(__dirname);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-    app.get('/', (req, res) => {
-      res.sendFile(__dirname + '/index.html');
-    });
-
-})
-
-
-
+app.post("/quotes", (req, res) => {
+  console.log("I would have posted if there wazza DB");
+});
+app.listen(3000, function () {
+  console.log("listening on 3000");
+});
